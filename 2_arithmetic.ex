03837863300bc7fa4e164880e706a61e8bb19e79
sjakobi <simon.jakobi@gmail.com>
2013-08-20 19:51:27 +0200

    2.01 fixed

--- 2_arithmetic.ex
+++ 2_arithmetic.ex
@@ -2,13 +2,22 @@ defmodule ArithmeticProblems do
 
   # 2.01
   # is_prime will quickly return false if n has small factors.
-  def is_prime(n) when is_integer(n) and n > 2 do
-    :math.sqrt(n) |>
-    trunc |>
-    (fn(x) -> 2..x end).() |>
-    Stream.filter(fn(x) -> rem(n, x) == 0 end) |>
-    Stream.take(1) |>
-    Enum.empty?
+  def is_prime(n) when is_integer(n) and n > 0 do
+    case n do
+      1 ->
+        false
+      2 ->
+        true
+      3 ->
+        true
+      _ ->
+        :math.sqrt(n) |>
+        trunc |>
+        (fn(x) -> 2..x end).() |>
+        Stream.filter(fn(x) -> rem(n, x) == 0 end) |>
+        Stream.take(1) |>
+        Enum.empty?
+    end
   end
 
 end
